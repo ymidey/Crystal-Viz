ab631c5a8c610371ceadc180954574802de9074d
ymidey <midey.yannick@gmail.com>
2023-10-31 14:33:14 +0100

    GSAP Animation + Axe X data SVG 📈

--- animation.js
+++ animation.js
@@ -1,6 +1,39 @@
-import gsap from 'gsap'
+// Importez GSAP directement depuis le CDN
+import { gsap } from 'gsap';
 
-gsap.from('.logo-container img', {
-    scale: 0,
-    x: 300
-})
+// Importez le TextPlugin
+import { TextPlugin } from 'gsap/TextPlugin';
+
+// Importez le ScrollTrigger
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+// Enregistrez le plugin ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.registerPlugin(TextPlugin, ScrollTrigger)
+
+    // Sélectionnez votre image
+    const img = document.querySelector(".logo-container img");
+
+    // Utilisez GSAP pour animer l'image
+    gsap.from(img, {
+        duration: 1, // Durée de l'animation (en secondes)
+        y: 100,
+        opacity: 0,
+        ease: "ease-out", // Type d'animation (facultatif)
+    });
+
+    gsap.from(".div-footer", {
+        x: 200,
+        opacity: 0,
+        scrollTrigger: {
+            trigger: div, // L'élément déclencheur
+            start: "top 80%", // L'animation commence lorsque 80% de l'élément est visible
+            end: "top 30%", // L'animation se termine lorsque 30% de l'élément est visible
+            scrub: true, // Permet d'ajuster l'animation en fonction du défilement
+        },
+    })
+
+
+});
