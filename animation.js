530195e4ea3fedf36b2b10d50f6ec2af1fe4fead
carolinedoung <dou.ca4@gmail.com>
2023-11-13 10:47:56 +0100

    :hammer: add images file and add square to the bars

--- animation.js
+++ animation.js
@@ -1,16 +1,14 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    gsap.registerPlugin(ScrollTrigger)
 
-    // Sélectionnez votre image
-    const img = document.querySelector(".logo-container img");
-    const div = document.querySelector('.div-footer')
-    // Utilisez GSAP pour animer l'image
-    gsap.from(img, {
-        duration: 1, // Durée de l'animation (en secondes)
-        y: 100,
+    const logoContainer = document.querySelector(".logo-container");
+
+    gsap.from(logoContainer, {
+        duration: 1.7,
+        y: 500,
         opacity: 0,
-        ease: "ease-out", // Type d'animation (facultatif)
+        ease: "ease-out",
     });
+
 
 });
