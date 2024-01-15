f9246b26c80e39787cb225dfe0e53f963ad5a495
Yannick MIDEY <77786954+ymidey@users.noreply.github.com>
2024-01-15 16:20:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -383,11 +383,11 @@ fetch('./FilmData.json')
             d3.select(".titre-nomines")
                 .data(anneeSelectionnee)
                 .append("h4")
-                .attr("class", "nominés")
+                .attr("id", "nominés")
                 .html(d => `Films nominés en ${d.AnnéeNomination}`);
 
 // Affichage des 34 premiers caractères du titre de chaque cristal pour pouvoir afficher 2 fois l'année 2009
-            d3.selectAll(".nominés").text(function () {
+            d3.select("#nominés").text(function () {
                 return d3.select(this).text().substring(0, 21)
             });
 
